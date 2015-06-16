67fd2dc31f12af95f682cd38e500c7f52986562e
sabagogolidze <sggol11@freeuni.edu.ge>
2015-06-16 17:40:24 +0400

    starting best aplication in the world

--- app/src/main/java/com/lookapp/activities/MainActivity.java
+++ app/src/main/java/com/lookapp/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.lookapp;
+package com.lookapp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.lookapp.R;
 
 
 public class MainActivity extends Activity {
