df43d75218b65ab9cb830add1ed464b9b509952e
sabagogolidze <sggol11@freeuni.edu.ge>
2015-06-23 17:51:33 +0400

    gitignore

---
+++ app/src/main/java/com/lookapp/settings/ServerConstants.java
@@ -0,0 +1,12 @@
+package com.lookapp.settings;
+
+/**
+ * Created by user on 23/06/2015.
+ */
+public class ServerConstants {
+
+    public static String SERVER_BASE_URL = "http://192.168.149.25:8080/LookAppServer";
+    public static String GET_SPOT_LIST_SERVLET = "SpotListServlet";
+    public static String GET_AVATAR_SERVLET = "AvatarServlet";
+
+}
